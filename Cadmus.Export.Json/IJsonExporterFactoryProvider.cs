a4597e561372eb048f278a0f4c9af5a07687f728
Myrmex <dfusi@hotmail.com>
2026-08-05 11:24:01 +0200

    Added JSON exporter factory provider

---
+++ Cadmus.Export.Json/IJsonExporterFactoryProvider.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+
+namespace Cadmus.Export.Json;
+
+/// <summary>
+/// Provider for <see cref="JsonExporterFactory"/>.
+/// </summary>
+public interface IJsonExporterFactoryProvider
+{
+    /// <summary>
+    /// Gets the factory.
+    /// </summary>
+    /// <param name="profile">The profile.</param>
+    /// <param name="additionalAssemblies">The optional additional assemblies
+    /// to load components from.</param>
+    /// <returns>Factory.</returns>
+    JsonExporterFactory GetFactory(string profile,
+        params Assembly[] additionalAssemblies);
+}
---
+++ Cadmus.Export.Json/StandardJsonExporterFactoryProvider.cs
@@ -0,0 +1,40 @@
+﻿using Cadmus.Export.Config;
+using Fusi.Microsoft.Extensions.Configuration.InMemoryJson;
+using Fusi.Tools.Configuration;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Cadmus.Export.Json;
+
+/// <summary>
+/// Standard implementation of <see cref="IJsonExporterFactoryProvider"/>.
+/// <para>Tag: <c>it.vedph.json-exporter-factory-provider.standard</c>.</para>
+/// </summary>
+[Tag("it.vedph.json-exporter-factory-provider.standard")]
+public sealed class StandardJsonExporterFactoryProvider : IJsonExporterFactoryProvider
+{
+    private static IHost GetHost(string config, Assembly[] assemblies)
+    {
+        return new HostBuilder()
+            .ConfigureServices((hostContext, services) =>
+                JsonExporterFactory.ConfigureServices(services, assemblies))
+            // extension method from Fusi library
+            .AddInMemoryJson(config)
+            .Build();
+    }
+
+    /// <summary>
+    /// Gets the factory for the specified profile.
+    /// </summary>
+    /// <param name="profile">The profile name.</param>
+    /// <param name="additionalAssemblies">Additional assemblies to consider.</param>
+    /// <returns>The <see cref="JsonExporterFactory"/> instance.</returns>
+    public JsonExporterFactory GetFactory(string profile,
+        params Assembly[] additionalAssemblies)
+    {
+        return new JsonExporterFactory(GetHost(profile, additionalAssemblies));
+    }
+}
