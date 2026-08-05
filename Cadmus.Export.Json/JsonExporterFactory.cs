2bb2936b417dab58e59e37f3478ff292b6711b46
Myrmex <dfusi@hotmail.com>
2026-08-05 10:06:18 +0200

    JSON exporter factory

--- Cadmus.Export.Json/JsonExporterFactory.cs
+++ Cadmus.Export.Json/JsonExporterFactory.cs
@@ -1,14 +1,16 @@
 ﻿using Cadmus.Export.Mapping;
-using Cadmus.Export.Renderers;
 using Fusi.Tools.Configuration;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
+using System.Text.Json;
 using System.Text.Json.Nodes;
 
-namespace Cadmus.Export.Config;
+namespace Cadmus.Export.Json;
 
 /// <summary>
 /// Factory for JSON exporter components. This factory is based on a JSON
@@ -17,6 +19,11 @@ namespace Cadmus.Export.Config;
 /// <item>
 /// <term><c>source/itemIdCollector</c></term>
 /// <description>configurable object for <see cref="IItemIdCollector"/>.
+/// </description>
+/// </item>
+/// <item>
+/// <term><c>source/itemJsonReader</c></term>
+/// <description>configurable object for <see cref="IItemJsonReader"/>.
 /// </description>
 /// </item>
 /// <item>
@@ -80,7 +87,7 @@ public sealed class JsonExporterFactory(IHost host) : ComponentFactory(host)
 
     /// <summary>
     /// Configures the container services to use components from
-    /// <c>Cadmus.Export</c>.
+    /// <c>Cadmus.Export.Json</c>.
     /// </summary>
     /// <param name="services">The services.</param>
     /// <param name="additionalAssemblies">The optional additional
@@ -94,8 +101,8 @@ public sealed class JsonExporterFactory(IHost host) : ComponentFactory(host)
         // https://simpleinjector.readthedocs.io/en/latest/advanced.html?highlight=batch#batch-registration
         Assembly[] assemblies =
         [
-            // Cadmus.Export
-            typeof(XsltJsonRenderer).Assembly
+            // Cadmus.Export.Json
+            typeof(JsonExporterFactory).Assembly
         ];
         if (additionalAssemblies?.Length > 0)
             assemblies = [.. assemblies, .. additionalAssemblies];
@@ -106,6 +113,7 @@ public sealed class JsonExporterFactory(IHost host) : ComponentFactory(host)
         {
             typeof(IItemIdCollector),
             typeof(IItemJsonReader),
+            typeof(IFluidFilter)
         })
         {
             foreach (Type t in GetAssemblyConcreteTypes(assemblies, it))
@@ -123,10 +131,79 @@ public sealed class JsonExporterFactory(IHost host) : ComponentFactory(host)
         GetComponent<IItemIdCollector>("Source/ItemIdCollector", true)!;
 
     /// <summary>
+    /// Gets the item JSON reader from <c>source/itemJsonReader</c>.
+    /// </summary>
+    /// <returns>The item JSON reader defined in this factory configuration.</returns>
+    public IItemJsonReader GetItemJsonReader() =>
+        GetComponent<IItemJsonReader>("Source/ItemJsonReader", true)!;
+
+    /// <summary>
     /// Gets the optional item part filter from <c>source/partFilter</c>.
     /// </summary>
     /// <returns>The item part filter defined in this factory configuration,
     /// or <c>null</c> if not defined.</returns>
     public ItemPartFilter? GetPartFilter() =>
         GetObjectFromJson<ItemPartFilter>("Source/PartFilter");
+
+    /// <summary>
+    /// Gets the optional template filters from <c>source/templateFilters</c>.
+    /// </summary>
+    /// <returns>The template filters defined in this factory configuration,
+    /// or <c>null</c> if not defined.</returns>
+    public IList<IFluidFilter> GetTemplateFilters() =>
+        GetComponents<IFluidFilter>("Source/TemplateFilters")
+            .Where(f => f != null).ToList()!;
+
+    /// <summary>
+    /// Gets the mappings from <c>mappings</c> (containing full mappings or just
+    /// mappings with name pointing to a named mapping) and <c>namedMappings</c>
+    /// (containing full mappings with a name to be referenced from mappings),
+    /// resolving links to named mappings.
+    /// </summary>
+    /// <returns>The list of full node mappings from this factory configuration.
+    /// </returns>
+    public IList<NodeMapping> GetMappings()
+    {
+        JsonSerializerOptions options = new(JsonSerializerDefaults.Web)
+        {
+            Converters = { new NodeMappingJsonConverter<JsonNodeMapping>() }
+        };
+
+        NodeMappingDocument document = new();
+
+        JsonNode? namedMappingsJson = GetJson("NamedMappings");
+        if (namedMappingsJson != null)
+        {
+            document.NamedMappings = namedMappingsJson
+                .Deserialize<Dictionary<string, NodeMapping>>(options) ?? [];
+        }
+
+        JsonNode? mappingsJson = GetJson("Mappings");
+        if (mappingsJson != null)
+        {
+            document.DocumentMappings = mappingsJson
+                .Deserialize<List<NodeMapping>>(options) ?? [];
+        }
+
+        return [.. document.GetMappings()];
+    }
+
+    /// <summary>
+    /// Gets a full top-level JSON exporter, composed from all its
+    /// dependencies as resolved from this factory's configuration (item
+    /// JSON reader, item ID collector, optional part filter, optional
+    /// template filters, and mappings).
+    /// </summary>
+    /// <returns>The exporter.</returns>
+    public JsonExporter? GetExporter()
+    {
+        JsonExporter exporter = new(GetItemJsonReader(), GetItemIdCollector())
+        {
+            PartFilter = GetPartFilter(),
+            TemplateFilters = [.. GetTemplateFilters()]
+        };
+        exporter.Mappings.AddRange(GetMappings());
+
+        return exporter;
+    }
 }
